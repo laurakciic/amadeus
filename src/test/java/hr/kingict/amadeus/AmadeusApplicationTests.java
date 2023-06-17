18ca16b9f60b30ccce8e96c0b79abc020fd68484
Laura Kovačić <laurakciic@gmail.com>
2023-06-17 10:19:59 +0200

    initial project add

---
+++ src/test/java/hr/kingict/amadeus/AmadeusApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.kingict.amadeus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmadeusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
